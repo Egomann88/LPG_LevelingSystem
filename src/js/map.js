43f1755a889e698e5a1598d9633364362ca2fbe7
Justin <justin@kauz.ch>
2022-04-20 07:38:57 +0200

    map js rnd number and css fixed

--- src/js/map.js
+++ src/js/map.js
@@ -1,27 +1,14 @@
 // onload page 
-window.onload = function addCss() {
+window.onload = function insertCssClasses() {
 
-    let allClasses = [
-        "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40",
-        "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20",
-        "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60",
-        "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40",
-        "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20",
-        "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60",
-        "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40",
-        "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20",
-        "mt-30", "mt-40", "mt-50", "mt-60", "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60",
-        "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60", "mt-30", "mt-40", "mt-50", "mt-60"
+    let rndClasses = [
+        "mt-10", "mt-20", "mt-30", "mt-40", "mt-50", "mt-60"
     ];
 
     for (let i = 0; i <= 20; i++) {
-        let id = 0;
-        let css = 0;
-        id = Math.floor((Math.random() * 20) + 1); // between 1 and 100
-        css = Math.floor((Math.random() * 100) + 1); // between 1 and 100
-        console.log("id", id);
-        console.log("css", css);
-        document.getElementById(id.toString().padStart(2, "0")).classList.add(allClasses[css - 1]);
+        let id = Math.floor((Math.random() * 20) + 1); // between 1 and 20
+        let css = Math.floor((Math.random() * 6) + 1); // between 1 and 6
+        document.getElementById(id.toString().padStart(2, "0")).classList.add(rndClasses[css - 1]);
     }
 }
 
